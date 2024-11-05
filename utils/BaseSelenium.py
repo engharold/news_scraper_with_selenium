68286b7e7b46df349b829301f225181b89a50574
Harold Alvarez <harold.alvarez@outlook.com>
2024-11-04 21:10:28 -0300

    First commit

--- utils/BaseSelenium.py
+++ utils/BaseSelenium.py
@@ -1,15 +1,4 @@
-# from RPA.Browser.Selenium import Selenium
-#
-# class ExtendedSelenium(Selenium):
-#
-#     def __init__(self, *args, **kwargs):
-#         Selenium.__init__(self, *args, **kwargs)
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 class BaseSelenium:
